b4af833d4ef1185ee8e43c18a6ef40e2fd9cdc34
傅庆发 <979338111@qq.com>
2022-04-25 15:00:04 +0800

    新增connection

--- gen.js
+++ gen.js
@@ -3,5 +3,5 @@ var fs = require('fs-extra');
 
 var distPath = path.resolve(process.cwd(), './dist');
 
-fs.moveSync(`${distPath}/dependency-ui/index.html`, `${distPath}/dependency-ui.html`);
+fs.moveSync(`${distPath}/dev-logs/index.html`, `${distPath}/dev-logs.html`);
 // fs.moveSync(`${distPath}/static/x`, `${distPath}/x`);

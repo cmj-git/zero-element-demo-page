bb213f96348e29fb0b4f32e4f8a7e564821ada29
傅庆发 <979338111@qq.com>
2022-05-05 17:31:23 +0800

    新增删除

--- src/.umi-production/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from 'C:/Users/1/Desktop/xiangmu2/zero-element-demo-page/node_modules/_@umijs_runtime@3.5.23@@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu','getInitialState','initialStateConfig','locale','request',],
-});
-
-export { plugin };
